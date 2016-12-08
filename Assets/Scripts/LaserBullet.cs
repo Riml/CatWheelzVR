3372a6ca404901f08e7fb261834c3dd6b1cafe3d
Ilmir Taychinov <riml@railgun-games.com>
2016-12-08 14:42:48 -0500

    lasers test

---
+++ Assets/Scripts/LaserBullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBullet : MonoBehaviour {
+
+    public float activeLaserTimeLenght = 0.5f;
+
+    void Start()
+    {
+        StartCoroutine(SelfDestruction());
+    }
+
+    IEnumerator SelfDestruction() {
+        yield return new WaitForSeconds(activeLaserTimeLenght);
+        Destroy(this.gameObject);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="Rat")  
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerShooting : NetworkBehaviour {
+
+    public GameObject laserPrefab;
+    public GameObject laserGun;//from which point we will be shooting
+
+    // Use this for initialization
+    void Start () {
+        MagnetSensor.OnCardboardTrigger += new MagnetSensor.CardboardTrigger(BeamsOfDeath);
+
+    }
+
+    public void BeamsOfDeath() {
+
+        GameObject laser = (GameObject)Instantiate(laserGun, Vector3.zero, Quaternion.identity);
+        NetworkServer.Spawn(laser);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
