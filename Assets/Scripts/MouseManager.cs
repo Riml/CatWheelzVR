e5103642736e04e2db33943a691ad4c1ab6f3b76
jbendercode <jbendercode@gmail.com>
2016-12-10 19:34:44 -0500

    Added MouseManagerScript and a test scene, extended and converted wander script from js to c sharp and added kill animation.

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MouseManager : NetworkBehaviour {
+
+	public float spawnTime;
+	public Transform[] spawnPoints;
+	public GameObject[] rats;
+	public float minScale;
+	public float maxScale;
+
+	int spawnPointIndex;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnRat", spawnTime, spawnTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SpawnRat(){
+		// Random rat prefab
+		int r = Random.Range(0, rats.Length);
+		spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+		// Generate random rat size
+		float ratScale = Random.Range (minScale, maxScale + 1f);
+		//float translateUp = tileHeight / 2f;
+
+		// Instantiate rat
+		GameObject enemy = (GameObject)Instantiate(rats[r], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+		// Transform local scale and translate tile appropriately
+		enemy.transform.localScale += new Vector3(ratScale, ratScale, ratScale);
+		//enemy.transform.position += new Vector3 (0, translateUp, 0);
+
+		//NetworkServer.Spawn(enemy);
+	}
+}
